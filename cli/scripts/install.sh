5607948e79f2c8100858c761c6d8038adc35036e
Adam Wathan <adam.wathan@gmail.com>
2016-08-08 15:08:35 -0400

    Use dynamic Composer hash

--- cli/scripts/install.sh
+++ cli/scripts/install.sh
@@ -22,7 +22,7 @@ if [[ ! $(which composer -A) ]]
 then
     echo "Installing Composer..."
     /usr/local/bin/php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    /usr/local/bin/php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" > /dev/null 2>&1
+    /usr/local/bin/php -r "if (hash_file('SHA384', 'composer-setup.php') === trim(file_get_contents('https://composer.github.io/installer.sig'))) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" > /dev/null 2>&1
     /usr/local/bin/php composer-setup.php > /dev/null 2>&1
     /usr/local/bin/php -r "unlink('composer-setup.php');"
 
